3ab56d7b1c37cf4df0523ad749282fd00dc760ec
Gokay Kucuk <serdargokaykucuk@gmail.com>
2020-03-25 20:28:19 +0100

    tests for document objects

--- tests/Index.test.ts
+++ tests/Index.test.ts
@@ -1,6 +1,6 @@
 import {GetAllDocuments, MatchParamOnIndex, GetRefsByParams} from "../src/Document";
 
-describe('index operations:', () => {
+describe.skip('index operations:', () => {
     test('reader can match param on idexes', async () => {
         const matchResult = await MatchParamOnIndex('deals', {
             key: 'url',
