b182acce8a90a933a4185af8a819d10e1e105d6e
gokaykucuk <serdargokaykucuk@gmail.com>
2020-03-26 16:49:16 +0100

    collection test

--- tests/Collections.test.ts
+++ tests/Collections.test.ts
@@ -1,9 +1,12 @@
-import { SchemaCollections } from "../src/Collections";
+import { SchemaCollections, SchemaCollectionNames } from "../src/Collections";
 
 describe("collection operations:", () => {
   test("can read collections", () => {
     const collections = SchemaCollections();
-    expect(collections.length).toBe(3);
+    expect(collections?.length).toBe(3);
   });
-  test("can get collection names", () => {});
+  test("can get collection names", () => {
+    const collectionNames = SchemaCollectionNames();
+    expect(collectionNames?.length).toBe(3);
+  });
 });
